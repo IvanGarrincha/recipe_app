2a1203ba22fcf3f3320924d8c2281dbc36b2fdb9
Ivan Garrido <ithsgarrido@gmail.com>
2020-12-29 18:38:09 +0100

    Category Entity tillagt.

---
+++ src/main/java/com/moose/recipe_app/repositories/CategpryRepository.java
@@ -0,0 +1,12 @@
+package com.moose.recipe_app.repositories;
+
+/**
+ * Created by Ivan Garrido on 2020-12-29.
+ * <p>
+ * Package: com.moose.recipe_app.repositories
+ */
+
+public interface CategpryRepository {
+    //$ ssh-keygen -t ed25519 -C "gorilla.garrido@gmial.com"
+
+}
